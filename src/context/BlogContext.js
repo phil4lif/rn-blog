98b26c8b3e973c5c32acb4cc52c6067fe63c32e9
phil cowan <philipjcowan1@gmail.com>
2020-06-19 14:27:15 -0700

    adds functionality for delete buttons

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -5,16 +5,22 @@ const blogReducer = (state, action) => {
     switch (action.type) {
         case 'add_blogpost':
             return [...state, { id: Math.floor(Math.random() * 99990), title: `Blog Post #${state.length+1}` }];
+        case 'delete_blogpost':
+            return state.filter((blogPost) => blogPost.id !== action.payload);
         default:
             return state;
         }
 }
 
-const addBlogPost = (dispatch) => {
+const addBlogPost = dispatch => {
     return () => {
     dispatch({ type: 'add_blogpost' })
-
     }
 };
+const deleteBlogPost = dispatch => {
+    return (id) => {
+        dispatch({ type: 'delete_blogpost', payload: id })
+    }
+}
 
-export const { Context, Provider } = createDataContext(blogReducer, { addBlogPost }, []);
+export const { Context, Provider } = createDataContext(blogReducer, { addBlogPost, deleteBlogPost }, []);
