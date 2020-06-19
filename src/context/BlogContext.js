68611366de3eb06ffa991bb63fd87f1bf57eabf2
phil cowan <philipjcowan1@gmail.com>
2020-06-19 13:49:11 -0700

    more context and reducers

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,18 +1,17 @@
-import React, { useState } from 'react'
+import React, { useReducer } from 'react'
+import createDataContext from './createDataContext';
 
-const BlogContext = React.createContext();
+const blogReducer = (state, action) => {
+    switch (action.type) {
+        case 'add_blogpost':
+            return [...state, { title: `Blog Post #${state.length+1}` }];
+        default:
+            return state;
+        }
+}
 
-export const BlogProvider =({ children }) => {
-    const [blogPosts, setBlogPosts] = useState([]);
-
-    const addBlogPost =() => {
-        setBlogPosts([...blogPosts, { title: `Blog Post #${blogPosts.length + 1}` }]);
-    };
-    return (
-    <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
-        {children}
-    </BlogContext.Provider>
-    )
+const addBlogPost = () => {
+    dispatch({ type: 'add_blogpost' })
 };
 
-export default BlogContext;
+export const { Context, Provider } = createDataContext(blogReducer, { addBlogPost }, []);
