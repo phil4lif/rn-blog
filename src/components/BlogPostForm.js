93bc40cdf93b8ef21eec65fc65ae3fb3a3e5d47b
phil cowan <philipjcowan1@gmail.com>
2020-06-20 20:38:00 -0700

    refactor

---
+++ src/components/BlogPostForm.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import {
+    View,
+    Text,
+    TextInput,
+    StyleSheet,
+    Button,
+    TouchableOpacity
+} from 'react-native';
+
+const BlogPostForm = () => {
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+    return (
+        <View>
+            <Text style={styles.label}>Blog Title</Text>
+            <TextInput 
+            style={styles.input}
+            value={title}
+            onChangeText={text => setTitle(text)}
+            />
+            <Text style={styles.label}>Body</Text>
+            <TextInput 
+            style={styles.input}
+            value={content}
+            onChangeText={text => setContent(text)}
+            />
+            <Button 
+            title="Save Blog Post"
+            />
+        </View>
+
+    )
+}
+
+const styles = StyleSheet.create({
+    input: {
+        fontSize: 18,
+        borderWidth: 1,
+        borderColor: 'black',
+        marginBottom: 15,
+        padding: 15,
+        margin: 5
+
+    },
+    label: {
+        fontSize: 20,
+        marginBottom: 5,
+        marginLeft: 15
+    }
+})
+
+export default BlogPostForm;
--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,13 +1,18 @@
-import React, { useContext, useState } from 'react';
-import { View, Text, StyleSheet, Button, TextInput } from 'react-native';
+import React, { useContext } from 'react';
+import { StyleSheet } from 'react-native';
+import { Context } from '../context/BlogContext';
+import BlogPostForm from '../components/BlogPostForm';
 
 const EditScreen = ({ navigation }) => {
+    const { state } = useContext(Context);
+    const blogPost = state.find(
+        blogPost => blogPost.id === navigation.getParam('id')
+        );
+
     return (
-        <View>
-            <Text>EditScreen - {navigation.getParam('id')}</Text>
-        </View>
+<BlogPostForm />
     )
-}
+};
 const styles = StyleSheet.create({
 
 })
