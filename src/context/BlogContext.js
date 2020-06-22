cab4d08e499bcf6eb5e0924a557c4232415a66e2
phil cowan <philipjcowan1@gmail.com>
2020-06-22 11:34:31 -0700

    changes deleteblogpost to be server delete

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -6,8 +6,6 @@ const blogReducer = (state, action) => {
     switch (action.type) {
         case 'get_blogposts':
             return action.payload;
-        case 'add_blogpost':
-            return [...state, { id: Math.floor(Math.random() * 99990), title: action.payload.title, content: action.payload.content }];
         case 'delete_blogpost':
             return state.filter((blogPost) => blogPost.id !== action.payload);
         case 'edit_blogpost':
@@ -31,14 +29,14 @@ const getBlogPosts = dispatch => {
 const addBlogPost = dispatch => {
     return async (title, content, cb) => {
         await jsonServer.post('/blogposts', { title, content });
-        // dispatch({ type: 'add_blogpost', payload: { title, content } });
         if (cb) {
             cb();
         }
     };
 };
 const deleteBlogPost = dispatch => {
-    return (id) => {
+    return async id => {
+        await jsonServer.delete(`/blogposts/${id}`)
         dispatch({ type: 'delete_blogpost', payload: id })
     };
 };
