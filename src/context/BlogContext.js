ea5cce358484586968c95106f013bb53ac15350e
phil cowan <philipjcowan1@gmail.com>
2020-06-22 09:35:07 -0700

    makes editpost fully functional with navigatepop

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -7,21 +7,39 @@ const blogReducer = (state, action) => {
             return [...state, { id: Math.floor(Math.random() * 99990), title: action.payload.title, content: action.payload.content }];
         case 'delete_blogpost':
             return state.filter((blogPost) => blogPost.id !== action.payload);
+        case 'edit_blogpost':
+            return state.map((blogPost) => {
+                return blogPost.id === action.payload.id
+                    ? action.payload
+                    : blogPost;
+            })
         default:
             return state;
-        }
+    }
 }
 
 const addBlogPost = dispatch => {
     return (title, content, cb) => {
-    dispatch({ type: 'add_blogpost', payload: {title, content} });
-    cb();
-    }
+        dispatch({ type: 'add_blogpost', payload: { title, content } });
+        if (cb) {
+            cb();
+        }
+    };
 };
 const deleteBlogPost = dispatch => {
     return (id) => {
         dispatch({ type: 'delete_blogpost', payload: id })
-    }
-}
+    };
+};
+const editBlogPost = dispatch => {
+    return (id, title, content, cb) => {
+        dispatch({
+            type: 'edit_blogpost', payload: { id, title, content }
+        });
+        if (cb) {
+            cb();
+        }
+    };
+};
 
-export const { Context, Provider } = createDataContext(blogReducer, { addBlogPost, deleteBlogPost }, [{title: 'test post', content: 'test content', id: 1}]);
+export const { Context, Provider } = createDataContext(blogReducer, { addBlogPost, deleteBlogPost, editBlogPost }, [{ title: 'test post', content: 'test content', id: 1 }]);
